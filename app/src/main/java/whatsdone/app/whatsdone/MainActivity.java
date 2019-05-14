709c223dae7809295d3be4dcb286c68a46afa999
krishani93 <krish93830@gmail.com>
2019-05-14 17:40:58 +0530

    implemented the tab layout

--- app/src/main/java/whatsdone/app/whatsdone/MainActivity.java
+++ app/src/main/java/whatsdone/app/whatsdone/MainActivity.java
@@ -1,13 +1,41 @@
 package whatsdone.app.whatsdone;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {R.drawable.group, R.drawable.task, R.drawable.settings};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Groups_Fragment(), "Groups");
+        adapter.addFragment(new Tasks_Fragment(), "Tasks");
+        adapter.addFragment(new Settings_Fragment(), "Settings");
+
+        viewPager.setAdapter(adapter);
+
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
+
 }
---
+++ app/src/main/java/whatsdone/app/whatsdone/TabAdapter.java
@@ -0,0 +1,52 @@
+package whatsdone.app.whatsdone;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabAdapter extends FragmentStatePagerAdapter
+{
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+    private Context context;
+
+    // private int[] tabIcons = {R.drawable.group, R.drawable.task, R.drawable.settings};
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    public void addFragment(Fragment fragment, String title)
+    {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitleList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+
+    }
+
