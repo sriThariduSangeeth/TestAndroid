0ba3d40b8a1178c1d9b23ad2b9d7eefddc99d510
Chandima Ranaweera <cranaweera@virtusa.com>
2019-06-05 00:18:41 +0530

    code verify screen added.

--- app/src/main/java/app/whatsdone/android/ui/activity/CodeVerificationActivity.java
+++ app/src/main/java/app/whatsdone/android/ui/activity/CodeVerificationActivity.java
@@ -1,16 +1,37 @@
 package app.whatsdone.android.ui.activity;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import app.whatsdone.android.R;
+import app.whatsdone.android.databinding.ActivityCodeVerificationBinding;
+import app.whatsdone.android.ui.presenter.CodeVerifyPresenter;
+import app.whatsdone.android.ui.presenter.CodeVerifyPresenterImpl;
+import app.whatsdone.android.ui.view.CodeVerifyView;
+import app.whatsdone.android.ui.viewmodel.CodeVerificationViewModel;
+import app.whatsdone.android.utils.Constants;
 
 // TODO: check if this class is a duplicate of PhoneNoVerificationActivity and remove
-public class CodeVerificationActivity extends AppCompatActivity {
+public class CodeVerificationActivity extends AppCompatActivity implements CodeVerifyView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_code_verification);
+        ActivityCodeVerificationBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_code_verification);
+        CodeVerificationViewModel model = new CodeVerificationViewModel();
+        model.setCode("123456");
+        CodeVerifyPresenter presenter = new CodeVerifyPresenterImpl();
+        presenter.setContext(this);
+        presenter.init(this, getIntent().getStringExtra(Constants.ARG_VERIFICATION_ID));
+        binding.setPresenter(presenter);
+        binding.setViewModel(model);
+    }
+
+    @Override
+    public void onVerified() {
+        Intent intent = new Intent(CodeVerificationActivity.this,ProfileCreationActivity.class);
+        startActivity(intent);
     }
 }
