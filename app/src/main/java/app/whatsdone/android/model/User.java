e23f83e5769cf814fc4a4c3a60c608c1b025cdc5
dtsangeeth <dtsangeeth@virtusa.com>
2019-06-07 13:38:50 +0530

    resovle merage confit

--- app/src/main/java/app/whatsdone/android/model/User.java
+++ app/src/main/java/app/whatsdone/android/model/User.java
@@ -58,11 +58,6 @@ public class User implements BaseEntity, IUser {
         return avatar;
     }
 
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-
     public boolean isEnableNotifications() {
         return enableNotifications;
     }
