5d7e448331a973a0d587047ac92627783465ad75
Chandima Ranaweera <chan4lk@gmail.com>
2019-06-12 15:13:13 +0530

    fixed status indicator

--- app/src/main/java/app/whatsdone/android/utils/DateUtil.java
+++ app/src/main/java/app/whatsdone/android/utils/DateUtil.java
@@ -1,12 +1,42 @@
 package app.whatsdone.android.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class DateUtil {
     public static Date getLastMinuteDate(Date date){
-        Long time = date.getTime();
-        Date newDate = new Date(time + time % (24 * 60 * 60 * 1000) - 1);
+        Calendar c = new GregorianCalendar();
+        c.setTime(date);
+        c.set(Calendar.HOUR_OF_DAY, 23); //anything 0 - 23
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 59);
+        Date newDate = c.getTime();
+
         return newDate;
+    }
+
+    public static boolean isEqual(Date date1, Date date2){
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
+        return fmt.format(date1).equals(fmt.format(date2));
+    }
+
+    public static Date addDays(Date date, int days)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, days); //minus number would decrement the days
+        return cal.getTime();
+    }
+
+    public static Date removeDays(Date date, int days)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, -days); //minus number would decrement the days
+        return cal.getTime();
     }
 
 }
