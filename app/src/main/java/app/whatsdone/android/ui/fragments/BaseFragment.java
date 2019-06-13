517c6ec247485777ec83029254a52dd9d6e1b325
Chandima Ranaweera <chan4lk@gmail.com>
2019-06-13 10:35:09 +0530

    update version

--- app/src/main/java/app/whatsdone/android/ui/fragments/BaseFragment.java
+++ app/src/main/java/app/whatsdone/android/ui/fragments/BaseFragment.java
@@ -30,6 +30,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baoyz.swipemenulistview.SwipeMenu;
