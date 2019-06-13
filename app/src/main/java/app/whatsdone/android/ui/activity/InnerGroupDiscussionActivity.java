0c7f1b0680b9294b9a4c0c6e758dbaaea80173fc
dtsangeeth <dtsangeeth@virtusa.com>
2019-06-13 17:41:40 +0530

    before getting memberd array

--- app/src/main/java/app/whatsdone/android/ui/activity/InnerGroupDiscussionActivity.java
+++ app/src/main/java/app/whatsdone/android/ui/activity/InnerGroupDiscussionActivity.java
@@ -3,13 +3,19 @@ package app.whatsdone.android.ui.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.Selection;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,6 +23,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 
+import com.otaliastudios.autocomplete.Autocomplete;
+import com.otaliastudios.autocomplete.AutocompleteCallback;
+import com.otaliastudios.autocomplete.AutocompletePolicy;
+import com.otaliastudios.autocomplete.AutocompletePresenter;
+import com.otaliastudios.autocomplete.CharPolicy;
 import com.stfalcon.chatkit.messages.MessageHolders;
 import com.stfalcon.chatkit.messages.MessageInput;
 import com.stfalcon.chatkit.messages.MessagesList;
@@ -33,25 +44,25 @@ import app.whatsdone.android.model.Group;
 import app.whatsdone.android.model.Message;
 import app.whatsdone.android.utils.GetCurrentDetails;
 import app.whatsdone.android.utils.MessageActivity;
+import app.whatsdone.android.utils.User;
+import app.whatsdone.android.utils.UserPresenter;
 
 import static android.R.*;
 
 public class InnerGroupDiscussionActivity extends MessageActivity implements MessageInput.InputListener,
         MessageInput.AttachmentsListener,
         MessageHolders.ContentChecker<Message>,
-        DialogInterface.OnClickListener  {
+        DialogInterface.OnClickListener   {
 
     private MessagesList messagesList;
     private ArrayAdapter<String> adapter;
     private static final byte CONTENT_TYPE_VOICE = 1;
     private GetCurrentDetails getCurrentDetails = new GetCurrentDetails();
     private MessageInput input;
-    private ListView viewIn;
-    public boolean hitAt = false;
-    public boolean hitHash = false;
-    public boolean hitSpace = false;
     public int charactorCount = 0;
     public int lessCharactorCount = 0;
+    private Autocomplete mentionsAutocompleteAt;
+    private Autocomplete mentionsAutocompleteHash;
 
     //this
     public int typingStartSize = 0;
@@ -69,7 +80,6 @@ public class InnerGroupDiscussionActivity extends MessageActivity implements Mes
 
 
         this.messagesList = (MessagesList) findViewById(R.id.messagesList);
-        this.viewIn = (ListView) findViewById(R.id.view_in);
         initAdapter();
         toolbar = (Toolbar) findViewById(R.id.toolbarInChat);
         setSupportActionBar(toolbar);
@@ -80,8 +90,6 @@ public class InnerGroupDiscussionActivity extends MessageActivity implements Mes
 
         adapter = new ArrayAdapter<String>(this, layout.simple_list_item_1, group.getMembers());
         adapter.setDropDownViewResource(layout.simple_spinner_dropdown_item);
-        viewIn.setAdapter(adapter);
-        viewIn.setVisibility(View.GONE);
 
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -92,181 +100,71 @@ public class InnerGroupDiscussionActivity extends MessageActivity implements Mes
             }
         });
 
-        input.getInputEditText().addTextChangedListener(new TextWatcher() {
+        float elevation = 6f;
+        Drawable backgroundDrawable = new ColorDrawable(Color.WHITE);
+        AutocompletePolicy policy = new CharPolicy('@');
+        AutocompletePolicy policy1 = new CharPolicy('#');
+        // Look for @mentions\
+        AutocompletePresenter<User> presenter = new UserPresenter(this);
+        AutocompleteCallback<User> callback = new AutocompleteCallback<User>() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                System.out.println("before text change : "+s);
+            public boolean onPopupItemClicked(Editable editable, User item) {
+                // Replace query text with the full name.
+                int[] range = CharPolicy.getQueryRange(editable);
+                if (range == null) return false;
+                int start = range[0];
+                int end = range[1];
+                String replacement = item.getUsername();
+                editable.replace(start, end, replacement);
+                // This is better done with regexes and a TextWatcher, due to what happens when
+                // the user clears some parts of the text. Up to you.
+                editable.setSpan(new StyleSpan(Typeface.BOLD), start, start+replacement.length(),
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                return true;
             }
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                System.out.println("change text : "+s);
+            public void onPopupVisibilityChanged(boolean shown) {
 
             }
+        };
 
-            @Override
-            public void afterTextChanged(Editable s) {
-                System.out.println("after text change : "+s);
+        mentionsAutocompleteAt = Autocomplete.<User>on(input.getInputEditText())
+                .with(elevation)
+                .with(backgroundDrawable)
+                .with(policy)
+                .with(presenter)
+                .with(callback)
+                .build();
 
-            }
-        });
+        mentionsAutocompleteHash =Autocomplete.<User>on(input.getInputEditText())
+                .with(elevation)
+                .with(backgroundDrawable)
+                .with(policy1)
+                .with(presenter)
+                .with(callback)
+                .build();
 
-
-        input.setTypingListener(new MessageInput.TypingListener() {
-            @Override
-            public void onStartTyping() {
-//                new Handler().postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        catchAt();
-//                    }
-//                }, 1000);
-//                typingStartSize = input.getInputEditText().getText().toString().length();
-
-            }
-            @Override
-            public void onStopTyping() {
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        catchAt();
-                    }
-                }, 1000);
-//                typingEndSize = input.getInputEditText().getText().toString().length();
-//                if(typingStartSize > typingEndSize){
-//                    String checkword = input.getInputEditText().getText().toString().substring(typingEndSize-1);
-//                    if(hitAt && checkword.equals("@")){
-//                        viewIn.setVisibility(View.VISIBLE);
-//                    }
-//                }else if(typingStartSize <= typingEndSize ){
-//                    //when insert characters
-//                    String checkword = input.getInputEditText().getText().toString().substring(typingStartSize-1);
-//                    if(checkword.indexOf("@") > -1 && !hitAt){
-//                        hitAt = true;
-//                        hitSpace = false;
-//                        atLenthList.add(typingEndSize);
-//                        viewIn.setVisibility(View.VISIBLE);
-//                        System.out.println("@ catch");
-//                    }else if(checkword.indexOf("#") > -1 && !hitHash){
-//                        hitHash = true;
-//                        hitSpace = false;
-//                        System.out.println("# catch");
-//                    }else if(checkword.indexOf(" ") > -1 && (hitAt || hitHash) ){
-//                        viewIn.setVisibility(View.GONE);
-//                        hitSpace = true;
-//                        hitAt = false;
-//                        hitHash = false;
-//                    }
-//                    if(hitAt && !checkword.equals("@")){
-//                        changeMemberAdapter(checkword, true);
-//                    }else if (hitAt && checkword.equals("@")) {
-//                        viewIn.setVisibility(View.VISIBLE);
-//                    }
+    }
+//    private void changeMemberAdapter(String mess , boolean set) {
+//
+//        if (set) {
+//            String ca = mess;
+//
+//            if (ca != null && !ca.isEmpty()) {
+//                List<String> listFound = new ArrayList<String>();
+//                for (String item : group.getMembers()) {
+//
+//                    if (item.contains(ca))
+//                        listFound.add(item);
 //                }
-
-            }
-        });
-
-        viewIn.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String selc = ((AppCompatTextView) view).getText().toString();
-                if(selc != null){
-                    stringBuilder.append(selc+" ");
-                }
-                input.getInputEditText().setText(input.getInputEditText().getText().toString()+stringBuilder);
-                viewIn.setVisibility(View.GONE);
-                hitSpace = false;
-                hitAt= false;
-                Selection.setSelection(input.getInputEditText().getText(),input.getInputEditText().length());
-                lessCharactorCount = input.getInputEditText().getText().length();
-
-            }
-        });
-
-    }
-
-
-    private void catchAt() {
-
-        boolean set = false;
-        String inputText = input.getInputEditText().getText().toString();
-        charactorCount = inputText.length();
-
-        try {
-            if(charactorCount > lessCharactorCount){
-                if (!hitAt) {
-                    if (inputText.substring(lessCharactorCount).indexOf("@") > -1) {
-                        viewIn.setVisibility(View.VISIBLE);
-                        adapter = new ArrayAdapter<String>(this, layout.simple_list_item_1, group.getMembers());
-                        adapter.setDropDownViewResource(layout.simple_spinner_dropdown_item);
-                        viewIn.setAdapter(adapter);
-                        hitSpace = false;
-                        lessCharactorCount = charactorCount;
-                        atLenthList.add(inputText.length());
-                        hitAt = true;
-                    }
-                } else if (inputText.length() < lessCharactorCount) {
-                    viewIn.setVisibility(View.GONE);
-                    atLenthList.remove(atLenthList.size() - 1);
-                    lessCharactorCount = atLenthList.get(atLenthList.size() - 1);
-                } else {
-                    if (!hitSpace && inputText.substring(lessCharactorCount).indexOf(" ") > -1 && inputText.length() > lessCharactorCount) {
-                        //after @ first space and break.
-                        hitAt = false;
-                        hitSpace = true;
-                        viewIn.setVisibility(View.GONE);
-                        lessCharactorCount = inputText.length();
-                    } else {
-                        changeMemberAdapter(inputText.substring(lessCharactorCount),true);
-                    }
-
-                }
-            }else {
-                lessCharactorCount--;
-                if (!atLenthList.isEmpty()) {
-                    if (inputText.length() == atLenthList.get(atLenthList.size() - 1) && !hitAt) {
-                        viewIn.setVisibility(View.VISIBLE);
-                        hitAt = true;
-                        hitSpace = false;
-                        atLenthList.remove(atLenthList.size() - 1);
-                        if (atLenthList.isEmpty()){lessCharactorCount = 0;}
-                        lessCharactorCount = atLenthList.get(atLenthList.size() - 1);
-                    }
-                }
-            }
-
-
-        }catch(
-                Exception e)
-
-        {
-            Log.d("ERROR", e.getMessage());
-        }
-
-    }
-
-
-    private void changeMemberAdapter(String mess , boolean set) {
-
-        if (set) {
-            String ca = mess;
-
-            if (ca != null && !ca.isEmpty()) {
-                List<String> listFound = new ArrayList<String>();
-                for (String item : group.getMembers()) {
-
-                    if (item.contains(ca))
-                        listFound.add(item);
-                }
-
-                adapter = new ArrayAdapter<String>(this, layout.simple_list_item_1, listFound);
-                viewIn.setAdapter(adapter);
-            }
-
-        }
-    }
+//
+//                adapter = new ArrayAdapter<String>(this, layout.simple_list_item_1, listFound);
+//                viewIn.setAdapter(adapter);
+//            }
+//
+//        }
+//    }
 
     @Override
     public void onSelectionChanged(int count) {
