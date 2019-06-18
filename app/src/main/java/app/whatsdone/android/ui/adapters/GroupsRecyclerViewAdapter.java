e5d01f49f836c3efe2bb5f05dce9d9d4d2620c01
Chandima Ranaweera <chan4lk@gmail.com>
2019-06-18 06:52:19 +0530

    changed icon generator

--- app/src/main/java/app/whatsdone/android/ui/adapters/GroupsRecyclerViewAdapter.java
+++ app/src/main/java/app/whatsdone/android/ui/adapters/GroupsRecyclerViewAdapter.java
@@ -3,15 +3,13 @@ package app.whatsdone.android.ui.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,34 +17,26 @@ import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.github.ivbaranov.mli.MaterialLetterIcon;
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 import java.util.Locale;
 
-import app.whatsdone.android.model.BaseEntity;
-import app.whatsdone.android.ui.activity.InnerGroupTaskActivity;
-import app.whatsdone.android.ui.fragments.InnerGroupTaskFragment;
 import app.whatsdone.android.R;
+import app.whatsdone.android.model.BaseEntity;
 import app.whatsdone.android.model.Group;
+import app.whatsdone.android.ui.activity.InnerGroupTaskActivity;
 import app.whatsdone.android.utils.DateUtil;
 import de.hdodenhof.circleimageview.CircleImageView;
 import timber.log.Timber;
 
-import static android.support.constraint.Constraints.TAG;
-import static com.github.ivbaranov.mli.MaterialLetterIcon.Shape.CIRCLE;
 
 public class GroupsRecyclerViewAdapter extends RecyclerView.Adapter<GroupsRecyclerViewAdapter.RecyclerViewHolder> {
-    public List<BaseEntity> groups;
+    private List<BaseEntity> groups;
     private Context context;
-    private TextView groupNameTextView, taskCount, discussionCount, toolbarTextView;;
-    private RecyclerView.LayoutManager linearLayoutManager;
-   // private GroupsRecyclerViewAdapter groupsRecyclerViewAdapter;
-
-
-    private RecyclerView.Adapter adapter;
 
     public GroupsRecyclerViewAdapter(List<BaseEntity> groups, Context context) {
         this.groups = groups;
@@ -93,20 +83,28 @@ public class GroupsRecyclerViewAdapter extends RecyclerView.Adapter<GroupsRecycl
         holder.dueDate.setText(DateUtil.formatted(group.getUpdatedDate(), null));
       //  holder.setIsRecyclable(false);
         holder.imageView.setTag(group);
+        TextDrawable placeHolder = TextDrawable.builder()
+                .buildRect(group.getGroupName().substring(0,1), Color.RED); // radius in px
+        ColorGenerator generator = ColorGenerator.MATERIAL; // or use DEFAULT
+
+        //int color1 = generator.getRandomColor();
+
+        int color2 = generator.getColor(group.getGroupName());
+        System.out.println(holder.imageView.getLayoutParams().height);
+        TextDrawable.IBuilder builder = TextDrawable.builder()
+                .beginConfig()
+                .withBorder(4)
+                .width(holder.imageView.getLayoutParams().width)
+                .height(holder.imageView.getLayoutParams().height)
+                .endConfig()
+                .rect();
+        TextDrawable ic1 = builder.build(group.getGroupName().substring(0,1), color2);
         try {
             if(!TextUtils.isEmpty(group.getAvatar())) {
-                holder.imageViewIcon.setVisibility(View.GONE);
-                holder.imageView.setVisibility(View.VISIBLE);
-                Picasso.get().load(group.getAvatar()).placeholder(R.drawable.user_group_man_woman3x).into(holder.imageView,getCallBack(holder.imageView) );
+                Picasso.get().load(group.getAvatar()).placeholder(ic1).into(holder.imageView,getCallBack(holder.imageView) );
             }else {
-                holder.imageViewIcon.setVisibility(View.VISIBLE);
-                holder.imageView.setVisibility(View.GONE);
-                holder.imageViewIcon
-                        .setShapeColor(context.getResources().getColor(R.color.default_icon_color));
-                holder.imageViewIcon.setShapeType(CIRCLE);
-                holder.imageViewIcon.setLetter(group.getGroupName().substring(0,1));
-                holder.imageViewIcon.setLetterColor(context.getResources().getColor(R.color.white));
-                holder.imageViewIcon.setLetterSize(20);
+
+                holder.imageView.setImageDrawable(ic1);
 
 
 
@@ -123,13 +121,12 @@ public class GroupsRecyclerViewAdapter extends RecyclerView.Adapter<GroupsRecycl
 
     }
 
-    public Callback getCallBack(final ImageView imageView) {
+    private Callback getCallBack(final ImageView imageView) {
         return new Callback() {
             @Override
             public void onSuccess() {
-                ScaleAnimation scale = new ScaleAnimation(0, 1, 0, 1, ScaleAnimation.RELATIVE_TO_SELF, .75f, ScaleAnimation.RELATIVE_TO_SELF, .75f);
-                scale.setDuration(400);
-                imageView.startAnimation(scale);
+                imageView.setAlpha(0f);
+                imageView.animate().setDuration(500).alpha(1f).start();
             }
 
             @Override
@@ -150,62 +147,28 @@ public class GroupsRecyclerViewAdapter extends RecyclerView.Adapter<GroupsRecycl
 
     public class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
-        private RecyclerView groupRecyclerView;
         public View view;
-
-
         private TextView groupNameTextView, taskCount, discussionCount, dueDate;
         private ImageView imageView, groupIcon;
-        private MaterialLetterIcon imageViewIcon;
         private Group group;
 
-
-
-        public RecyclerViewHolder(@NonNull View itemView) {
+        RecyclerViewHolder(@NonNull View itemView) {
             super(itemView);
 
             groupNameTextView = itemView.findViewById(R.id.group_text);
             taskCount = itemView.findViewById(R.id.unread_tasks_counter);
             discussionCount = itemView.findViewById(R.id.unread_discussion_counter);
-            groupRecyclerView = itemView.findViewById(R.id.group_recycler_view);
             imageView =(CircleImageView) itemView.findViewById(R.id.image_view_group);
-            imageViewIcon = itemView.findViewById(R.id.image_view_group_icon);
             groupIcon = itemView.findViewById(R.id.group_chat_icon);
             dueDate = itemView.findViewById(R.id.date);
-            if(imageView.getDrawable() == null)
-            {
-//                MaterialLetterIcon icon = new MaterialLetterIcon.Builder(context) //
-//                        .shapeColor(context.getResources().getColor(R.color.colorAccent))
-//                        .shapeType(CIRCLE)
-//                        .letter("S")
-//                        .letterColor(context.getResources().getColor(R.color.white))
-//                        .letterSize(26)
-//                        .lettersNumber(1)
-//
-//                        .initials(false)
-//                        .initialsNumber(2)
-//                        .create();
-                imageViewIcon
-                        .setShapeColor(context.getResources().getColor(R.color.colorAccent));
-                imageViewIcon.setShapeType(CIRCLE);
-                imageViewIcon.setLetter("G");
-                imageViewIcon.setLetterColor(context.getResources().getColor(R.color.white));
-                imageViewIcon.setLetterSize(26);
-                imageViewIcon.setVisibility(View.VISIBLE);
-                imageView.setVisibility(View.GONE);
-                Drawable defaultImage= itemView.getResources().getDrawable(R.drawable.user_group_man_woman3x);
-            }
-
 
             itemView.setOnClickListener(v -> {
-                group.setTeamImage(getImageData(imageView));
+                //group.setTeamImage(getImageData(imageView));
 
                 AppCompatActivity activity = (AppCompatActivity) itemView.getContext();
                 Intent intent = new Intent(activity, InnerGroupTaskActivity.class);
                 intent.putExtra("group", group);
                 activity.startActivity(intent);
-
-
             });
 
         }
@@ -219,7 +182,7 @@ public class GroupsRecyclerViewAdapter extends RecyclerView.Adapter<GroupsRecycl
         }
     }
 
-    public Bitmap getImageData(ImageView imageView){
+    private Bitmap getImageData(ImageView imageView){
         //Get the data from an ImageView as bytes
         if (imageView == null) return null;
         Drawable drawable = imageView.getDrawable();
@@ -227,8 +190,7 @@ public class GroupsRecyclerViewAdapter extends RecyclerView.Adapter<GroupsRecycl
         if(drawable == null) return null;
         imageView.setDrawingCacheEnabled(true);
         imageView.buildDrawingCache();
-        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-        return  bitmap;
+        return ((BitmapDrawable) drawable).getBitmap();
     }
 
     @Override
