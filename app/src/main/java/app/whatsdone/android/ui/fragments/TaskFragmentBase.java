1511bca78569dbb05f808ee8e72fd69cc321b3cd
Chandima Ranaweera <chan4lk@gmail.com>
2019-06-20 04:14:19 +0530

    check list styled

--- app/src/main/java/app/whatsdone/android/ui/fragments/TaskFragmentBase.java
+++ app/src/main/java/app/whatsdone/android/ui/fragments/TaskFragmentBase.java
@@ -18,6 +18,7 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -197,6 +198,10 @@ public abstract class TaskFragmentBase extends Fragment implements ContactPicker
             task.setUpdatedDate(new Date());
             task.setAssignedUserImage(UrlUtils.getUserImage(task.getAssignedUser()));
             save();
+            getActivity().getWindow().setSoftInputMode(
+                    WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+            );
+
             getActivity().onBackPressed();
 
 
